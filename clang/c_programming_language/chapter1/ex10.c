b9ee48d118c072823cced50539ddc5f7db2f50e5
Enes Emre Aydogan <eeay02@gmail.com>
2025-07-08 12:24:57 +0300

    feat: chapter1/ex10

---
+++ clang/c_programming_language/chapter1/ex10.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int c = EOF;
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            printf("\\t");
+        } else if (c == '\b') {
+            printf("\\b");
+        } else if (c == '\\') {
+            printf("\\\\");
+        } else {
+            putchar(c);
+        }
+    }
+}
