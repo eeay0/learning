cca884e47f0a0344ed5251b9a1420280f2592188
Enes Emre Aydogan <eeay02@gmail.com>
2025-07-10 14:32:16 +0300

    chor: chapter1/ex17

---
+++ c_programming_language/chapter1/ex17.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX 1000
+#define MIN 10
+
+int getline(char s[], int lim);
+
+int main(void) {
+    char line[1000];
+    int len;
+
+    while ((len = getline(line, MAX)) > MIN) {
+        printf("Line size is: %d\n", len);
+        printf("%s", line);
+    }
+}
+
+int getline(char s[], int lim) {
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = (char) c;
+    if (c == '\n') {
+        s[i] = (char) c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
